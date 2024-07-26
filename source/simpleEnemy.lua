e2b299940a819eb22664bf2cb2755dd7433f255a
tage <tagetjerngren@gmail.com>
2024-07-27 00:58:04 +0200

    Modified the level and made it so the enemy won't shoot you if you're inactive

--- source/simpleEnemy.lua
+++ source/simpleEnemy.lua
@@ -26,7 +26,7 @@ function SimpleEnemy:update()
 	local toPlayer = self.player.PhysicsComponent.Position - self.position
 	if toPlayer:magnitude() < 250 and self.cooldown >= 15 then
 		local _, _, _, n = self:checkCollisions(self.player.x, self.player.y)
-		if n < 1 then
+		if n < 1 and self.player.bActive then
 			Bullet(self.x + toPlayer:normalized().x * 30, self.y + toPlayer:normalized().y * 30, toPlayer:normalized() * 5)
 			self.cooldown = 0
 		end
