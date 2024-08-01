ce0b7ef74d414606cd3767b962143701f6c0c0b4
tage <tagetjerngren@gmail.com>
2024-08-01 11:49:01 +0200

    Resized grid in ldtk for more modularity and added resizable one way doors

--- source/scripts/entities/oneWayDoor.lua
+++ source/scripts/entities/oneWayDoor.lua
@@ -7,14 +7,15 @@ import "CoreLibs/nineslice"
 class('OneWayDoor').extends(gfx.sprite)
 
 local Orientation = {
-	NORTH = 1,
-	EAST = 2,
-	SOUTH = 3,
-	WEST = 4
+	["North"] = 2,
+	["East"] = 2,
+	["South"] = 2,
+	["West"] = 2,
 }
 
 function OneWayDoor:init(x, y, entity, orientation)
 	self.entity = entity
+	self.Blocking = entity.fields.Blocking
 	printTable(entity)
 	-- NOTE: This says which way the one way door is oriented
 	self:setCenter(0, 0)
@@ -28,7 +29,7 @@ end
 function OneWayDoor:open()
 	local sprite = gfx.image.new(self.entity.size.width, 32)
 	gfx.lockFocus(sprite)
-	local ns = gfx.nineSlice.new("images/OneWayDoorOpen", 5, 5, 21, 5)
+	local ns = gfx.nineSlice.new("images/OneWayDoor", 5, 5, 21, 21)
 	ns:drawInRect(0, 0, self.entity.size.width, 16)
 	gfx.unlockFocus()
 	self:setImage(sprite)
@@ -39,18 +40,22 @@ end
 function OneWayDoor:close()
 	local sprite = gfx.image.new(self.entity.size.width, 32)
 	gfx.lockFocus(sprite)
-	local ns = gfx.nineSlice.new("images/OneWayDoorClosed", 5, 3, 21, 2)
-	ns:drawInRect(0, 0, self.entity.size.width, 5)
+	local ns = gfx.nineSlice.new("images/OneWayDoorSmall", 3, 3, 25, 25)
+	if self.bBottomUp then
+		ns:drawInRect(0, 16, self.entity.size.width, 5)
+	else
+		ns:drawInRect(0, 0, self.entity.size.width, 5)
+	end
 	gfx.unlockFocus()
 	self:setImage(sprite)
-	self:setCollideRect(0, 4, self.entity.size.width, 1)
+	self:setCollideRect(0, 6, self.entity.size.width, 1)
 	self.bOpen = false
 end
 
 function OneWayDoor:update()
 	local width, _ = self:getSize()
 	if not self.bOpen then
-		local collisionsInOpen = gfx.sprite.querySpritesInRect(self.x, self.y, width, 4)
+		local collisionsInOpen = gfx.sprite.querySpritesInRect(self.x, self.y, width, 6)
 		if #collisionsInOpen > 0 then
 			self:open()
 		end
