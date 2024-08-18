66fe10ca53063c6941556bc02dd31550a9e4458d
tage <tagetjerngren@gmail.com>
2024-08-18 21:15:43 +0200

    Started to make the option box a bit prettier

--- source/scripts/optionBox.lua
+++ source/scripts/optionBox.lua
@@ -10,30 +10,38 @@ function OptionBox:init(prompt, options, callback)
 	SceneManager.player.bActive = false
 	SceneManager.water.bActive = false
 
+	self:setIgnoresDrawOffset(true)
+	self:moveTo(200, 120)
+
 	self.options = options
 	self.prompt = prompt
 	self.callback = callback
 
-	self:setImage(gfx.image.new(400, 240))
-	self:setCenter(0, 0)
+	self:setImage(gfx.image.new(340, 180))
 
-	self.grid = pd.ui.gridview.new((400 - 60 - 20 - 4 * #options)/#options, 50)
+	self.grid = pd.ui.gridview.new((400 - 60 - 20 - 4 * #options)/#options, 60)
 	self.grid:setNumberOfColumns(#options)
 	self.grid:setNumberOfRows(1)
 	self.grid:setCellPadding(2, 2, 2, 2)
 	self.grid:setContentInset(10, 10, 10, 10)
+
+	self.grid.backgroundImage = gfx.nineSlice.new("images/WallResizable", 5, 5, 6, 6)
+
 	local option = options
+	local ns = gfx.nineSlice.new("images/WallResizable", 5, 5, 6, 6)
+	local nsBlank = gfx.nineSlice.new("images/OneWayDoor", 5, 5, 22, 22)
+
 	function self.grid:drawCell(section, row, column, selected, x, y, width, height)
 		gfx.setColor(gfx.kColorBlack)
 		if selected then
+			ns:drawInRect(x, y + 50, width, height)
 			gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
-			gfx.fillRect(x, y, width, height)
 			gfx.setColor(gfx.kColorWhite)
-			gfx.drawTextInRect(option[column], x, y + (height/2) - 10 + 3 * math.sin(7 * pd.getElapsedTime()), width, height, nil, nil, kTextAlignment.center)
+			gfx.drawTextInRect(option[column], x, y + (height/2) - 10 + 50 + 3 * math.sin(7 * pd.getElapsedTime()), width, height, nil, nil, kTextAlignment.center)
 		else
+			nsBlank:drawInRect(x, y + 50, width, height)
 			gfx.setImageDrawMode(gfx.kDrawModeCopy)
-			gfx.drawRect(x, y, width, height)
-			gfx.drawTextInRect(option[column], x, y + (height/2) - 10, width, height, nil, nil, kTextAlignment.center)
+			gfx.drawTextInRect(option[column], x, y + (height/2) - 10 + 50, width, height, nil, nil, kTextAlignment.center)
 		end
 	end
 	self:setZIndex(10)
@@ -53,14 +61,11 @@ function OptionBox:update()
 		self.selection = column
 		self.callback(self.selection, self.options[column])
 	end
-	local offsetX, offsetY = gfx.getDrawOffset()
-	self:moveTo(30 - offsetX, 30 - offsetY)
 
 	gfx.lockFocus(self:getImage())
-	gfx.setColor(gfx.kColorWhite)
-	gfx.fillRect(0, 0, 400 - 60, 240 - 60)
 	self.grid:drawInRect(0, 0, 400 - 60, 240 - 60)
-	gfx.drawTextInRect(self.prompt, (400 - 60)/2, 80, 100, 100, nil, nil, kTextAlignment.center)
+	gfx.setImageDrawMode(gfx.kDrawModeFillWhite)
+	gfx.drawTextAligned(self.prompt, (400 - 60)/2, 20, kTextAlignment.center)
 	-- self.grid:drawInRect(30 - offsetX, 30 - offsetY, 400 - 60, 240 - 60)
 	gfx.unlockFocus()
 end
